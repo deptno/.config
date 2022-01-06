85705223f59f8f9b129988a74b68f7305f7686d3
deptno <deptno@gmail.com>
2022-01-07 02:46:37 +0900

    update .finichy

--- .finicky.js
+++ .finicky.js
@@ -13,4 +13,13 @@ module.exports = {
       match: /^https:\/\/console\.firebase\.google\.com\/.*$/,
       browser: "Google Chrome"
     },
+    {
+      match: /^https:\/\/appstoreconnect\.apple\.com.*/,
+      browser: "Google Chrome"
+    },
+    {
+      match: /^https:\/\/developer\.apple\.com.*/,
+      browser: "Google Chrome"
+    }
+  ]
 }
