41f55e506f750a620ff5d08b7ba58c1f7d4d4313
deptno <deptno@gmail.com>
2022-01-14 23:00:58 +0900

    finicky route url base on jwt

--- .finicky.js
+++ .finicky.js
@@ -3,23 +3,81 @@ module.exports = {
   handlers: [
     {
       match: /zigbang/,
-      browser: "Google Chrome"
+      browser: "Google Chrome",
     },
     {
       match: /^https:\/\/meet\.google\.com\/.*$/,
-      browser: "Google Chrome"
+      browser: "Google Chrome",
+    },
+    {
+      match: /^https:\/\/docs\.google\.com\/.*$/,
+      browser: "Google Chrome",
     },
     {
       match: /^https:\/\/console\.firebase\.google\.com\/.*$/,
-      browser: "Google Chrome"
+      browser: "Google Chrome",
     },
     {
       match: /^https:\/\/appstoreconnect\.apple\.com.*/,
-      browser: "Google Chrome"
+      browser: "Google Chrome",
     },
     {
       match: /^https:\/\/developer\.apple\.com.*/,
-      browser: "Google Chrome"
-    }
+      browser: "Google Chrome",
+    },
+    {
+      match: /^https:\/\/app\.zeplin\.io.*/,
+      browser: "Google Chrome",
+    },
+    {
+      match: ({ url }) => {
+        const queries = getUrlParams(url.search)
+        const payload = parseJwt(queries.login_hint)
+
+        if (payload) {
+          const { iss, sub } = payload
+
+          if (iss === 'https://slack.com') {
+            return payload['https://slack.com/team_id'] === 'T06BALYG5'
+          }
+        }
+        return false
+      },
+      browser: "Google Chrome",
+    },
   ]
+}
+
+const parseJwt = (token) => {
+  try {
+    const payload = token.split('.')[1]
+    return JSON.parse(atob(payload));
+  } catch (e) {
+  }
+}
+const getUrlParams = (search) => {
+  return search
+    .split('&')
+    .reduce((queries, hash) => {
+      const [key, val] = hash.split('=')
+      queries[key] = decodeURIComponent(val)
+      return queries
+    }, {})
+}
+const atob = (input) => {
+  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
+  const str = (String (input)).replace (/[=]+$/, '');
+
+  if (str.length % 4 === 1) {
+    throw new InvalidCharacterError ("'atob' failed: The string to be decoded is not correctly encoded.");
+  }
+  for (
+    var bc = 0, bs, buffer, idx = 0, output = '';
+    buffer = str.charAt (idx++);
+    ~buffer && (bs = bc % 4 ? bs * 64 + buffer : buffer,
+      bc++ % 4) ? output += String.fromCharCode (255 & bs >> (-2 * bc & 6)) : 0
+  ) {
+    buffer = chars.indexOf (buffer);
+  }
+  return output;
 }
