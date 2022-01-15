2ed03a004a8c5ca939359c7a5aa2db39ae819603
deptno <deptno@gmail.com>
2022-01-16 04:40:41 +0900

    add tagbar

---
+++ .config/nvim/lua/user/tagbar.lua
@@ -0,0 +1,39 @@
+vim.cmd [[
+augroup tagbar_vimwiki
+  autocmd BufRead,BufNewFile *.md TagbarOpen
+  autocmd VimLeavePre *.md TagbarClose
+augroup END
+
+set tags+=~/workspace/src/github.com/deptno/deptno.github.io.wiki
+set tags+=~/workspace/src/github.com/deptno/wiki
+
+let g:tagbar_type_vimwiki = {
+  \ 'ctagstype'	: 'markdown',
+  \ 'kinds'		: [
+    \ 'c:.1:0:0',
+    \ 's:.2:0:1',
+    \ 'S:.3:0:1',
+    \ 't:.4:0:1',
+    \ 'T:.5:0:1',
+    \ 'u:.6:0:1',
+  \ ],
+  \ 'sro'			: '""',
+  \ 'kind2scope'	: {
+    \ 'c' : 'chapter',
+    \ 's' : 'section',
+    \ 'S' : 'subsection',
+    \ 't' : 'subsubsection',
+    \ 'T' : 'l4subsection',
+  \ },
+  \ 'scope2kind'	: {
+    \ 'chapter' : 'c',
+    \ 'section' : 's',
+    \ 'subsection' : 'S',
+    \ 'subsubsection' : 't',
+    \ 'l4subsection' : 'T',
+  \ },
+  \ 'sort': 0,
+\ }
+
+nmap ;1 :TagbarToggle<cr>
+]]
