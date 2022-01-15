a519946c877147cf7bb6215d7887ecbbb1bf055a
deptno <deptno@gmail.com>
2022-01-16 05:49:26 +0900

    update config treesitter, gitsigns prettier, eslint with local

--- .config/nvim/lua/user/lsp/null-ls.lua
+++ .config/nvim/lua/user/lsp/null-ls.lua
@@ -3,16 +3,20 @@ if not null_ls_status_ok then
 	return
 end
 
--- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
 local formatting = null_ls.builtins.formatting
--- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
 local diagnostics = null_ls.builtins.diagnostics
+local code_actions = null_ls.builtins.code_actions
 
 null_ls.setup({
 	debug = false,
 	sources = {
-		formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
+		formatting.prettier.with({
+        only_local = "node_modules/.bin",
+    }),
 		formatting.stylua,
-    diagnostics.eslint,
+    diagnostics.eslint.with({
+        only_local = "node_modules/.bin",
+    }),
+    code_actions.gitsigns,
 	},
 })
