ea30e0a78ba3f3dfcdf4f157fcba782a8e7553c5
deptno <deptno@gmail.com>
2022-01-26 23:45:10 +0900

    comment: 미확인 코드 주석

--- .config/nvim/lua/user/auto_commands.lua
+++ .config/nvim/lua/user/auto_commands.lua
@@ -1,3 +1,5 @@
 vim.cmd "autocmd FileType lspinfo nnoremap q :q<cr>"
 vim.cmd "autocmd FileType help nnoremap q :q<cr>"
 vim.cmd "autocmd FileType qf nnoremap q :q<cr>"
+-- @todo clipboard 사용을 위한 코드, 미확인 
+-- vim.cmd "autocmd TextYankPost * call system(\"pbcopy\", @\")"
