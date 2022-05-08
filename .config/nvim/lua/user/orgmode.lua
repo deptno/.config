07ad9acb533c28596bd5090782f75879a62ff4cc
deptno <deptno@gmail.com>
2022-05-09 02:38:13 +0900

    chore: update orgmode global hotkey

--- .config/nvim/lua/user/orgmode.lua
+++ .config/nvim/lua/user/orgmode.lua
@@ -3,7 +3,13 @@ if not status_ok then
   return
 end
 
-require("orgmode").setup {
+orgmode.setup {
   org_agenda_files = {'~/workspace/src/github.com/deptno/org/*'},
   org_default_notes_file = '~/workspace/src/github.com/deptno/org/refile.org',
+  mappings = {
+    global = {
+      org_agenda = {'<space>oa', '<leader>oa'},
+      org_capture = {'<space>oc', '<leader>oc'},
+    }
+  }
 }
