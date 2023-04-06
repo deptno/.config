0b2f235e2a3452fb6043f4e607a6bf1968804030
deptno <deptno@gmail.com>
2023-04-06 11:36:56 +0900

    neovim: add name for vimwiki

--- .config/nvim/lua/user/vimwiki.lua
+++ .config/nvim/lua/user/vimwiki.lua
@@ -2,12 +2,14 @@ vim.g.vimwiki_auto_chdir = 1
 vim.g.vimwiki_conceallevel = 0
 vim.g.vimwiki_list = {
   {
+    name = 'public',
     path = '~/workspace/src/github.com/deptno/deptno.github.io.wiki',
     ext  = '.md',
     diary_rel_path = 'diary',
     diary_index = 'index',
   },
   {
+    name = 'private',
     path = '~/workspace/src/github.com/deptno/wiki',
     ext  = '.md',
     diary_rel_path = 'diary',
