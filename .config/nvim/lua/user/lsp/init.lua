b3519e43b8320265752ba85b6b79d4663e2a468e
deptno <deptno@gmail.com>
2023-09-03 16:59:08 +0900

    set keymap for diagnostic

--- .config/nvim/lua/user/lsp/init.lua
+++ .config/nvim/lua/user/lsp/init.lua
@@ -6,3 +6,6 @@ end
 require 'user.lsp.lsp-installer'
 require 'user.lsp.handlers'.setup()
 require 'user.lsp.null-ls'
+
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
