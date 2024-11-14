f5890cce7935d9010b2f5e2ad2504bbd21da9229
deptno <deptno@gmail.com>
2024-11-14 19:52:37 +0900

    gx.nvim import 사용 시작

---
+++ .config/nvim/lua/plugins/gx.nvim.lua
@@ -0,0 +1,17 @@
+return {
+  "deptno/gx.nvim",
+  dir = "/Users/deptno/workspace/src/github.com/deptno/gx.nvim",
+  opts = {},
+  keys = {
+    {
+      "gx",
+      "<cmd>Gx<cr>",
+      mode = {
+        "n",
+        "v",
+      },
+      desc = "gx.nvim",
+    },
+  },
+  dev = true,
+}
