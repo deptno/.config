04a7cb548515d2ea7eb52e77ba779fe3d11d3991
deptno <deptno@gmail.com>
2024-12-12 16:25:56 +0900

    노멀모드에서 키보드 튀지 않도록 수정

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -5,5 +5,35 @@
 vim.o.timeout = true
 vim.o.timeoutlen = 150
 vim.o.jumpoptions = "stack"
-
 vim.g.mapleader = ";"
+-- normal 모드에서 한글 입력되는 것 방지
+vim.o.langmap = table.concat({
+  "ㅂq",
+  "ㅈw",
+  "ㅉW",
+  "ㄷe",
+  "ㄸE",
+  "ㄱr",
+  "ㅅt",
+  "ㅛy",
+  "ㅕu",
+  "ㅑi",
+  "ㅐo",
+  "ㅔp",
+  "ㅁa",
+  "ㄴs",
+  "ㅇd",
+  "ㄹf",
+  "ㅎg",
+  "ㅗh",
+  "ㅓj",
+  "ㅏk",
+  "ㅣl",
+  "ㅋz",
+  "ㅌx",
+  "ㅊc",
+  "ㅍv",
+  "ㅠb",
+  "ㅜn",
+  "ㅡm",
+}, ",")
